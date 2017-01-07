b4f2885e65544cc9f1a5663681ae5f3e9d2af68c
Buddha Jyothiprasad <jyothiprasadb@gmail.com>
2017-01-07 20:58:22 +0530

    swift problem set

---
+++ ios-nd-swift-problem-set-master/SwiftProblemSet.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ [Table of Contents](Table%20of%20Contents) | [Previous](@previous)
+ ****
+ */
+import Foundation
+//: ## Challenge
+//: **This exercise is completely optional and is not required for completing the Swift Problem Set.**
+//: ### Challenge 1
+//: Mystery code! What does this code do? Briefly, using comments, describe what is happening in each line. **Hint**: You may need to look up [Int initializers](http://stackoverflow.com/questions/30739460/toint-removed-in-swift-2).
+// declaring an array
+let array = ["A", "13", "B", "5", "87", "t", "41"]
+var sum = 0
+for string in array {
+    if Int(string) != nil {
+        // trying to convert the number in string format to an integer
+        let intToAdd = Int(string)!
+        sum += intToAdd
+    }
+}
+print(sum)
+
+/*:
+ ****
+ [Table of Contents](Table%20of%20Contents) | [Previous](@previous)
+ */
